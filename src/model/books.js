e6cd1eb0f0d2446920ffe7f245a27ed0f9c00753
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-17 16:49:26 +0100

    added GET and headers to query function

--- src/model/books.js
+++ src/model/books.js
@@ -1,11 +1,19 @@
 const BASE_URL = 'https://openlibrary.org';
+const params = new URLSearchParams();
 const headers = new Headers({
     "User-Agent": "CPNV_projet_frontend/1.0 (py58wpb@eduvaud.ch)"
 }); //header required by OpenLibrary API so that requests are not blocked
 
 export async function searchBooks(query) {
+    if (!query || !query.trim()) return []; // Avoid empty queries
     try {
-        const response = await fetch(`${BASE_URL}/search.json?q=${encodeURIComponent(query)}`);
+        const response = await fetch(
+            `${BASE_URL}/search.json?${new URLSearchParams(query)}&limit=10`,
+            {
+                method: 'GET',
+                headers: headers
+            }
+        )
         if (!response.ok) {
             throw new Error(`API error: ${response.status}`);
         }
