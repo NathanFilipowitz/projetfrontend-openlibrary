6ce77481ffc90291217ff73a8800b0de0260304f
Fabian <rostellofabian@gmail.com>
2025-12-08 16:20:38 +0100

    feat: added localStorage to api

--- src/db/db.js
+++ src/db/db.js
@@ -1,38 +1,25 @@
-import fs from 'fs';
+const DATA_KEY = 'openlibrary';
 
-export function readDB(nameDB) {
-    try {
-        const data = fs.readFileSync(nameDB, 'utf8');
-        return JSON.parse(data);
-    } catch (err) {
-        console.error("Failed to read data:", err);
-        return null;
-    }
+const db = {
+    readDB: () => {
+        try {
+            const data = localStorage.getItem(DATA_KEY);
+
+            return data ? JSON.parse(data) : []; // Opérateur ternaire
+        } catch (err) {
+            console.error("Failed to read data from localStorage:", err);
+            return [];
+        }
+    },
+    writeDB: (data) => {
+        if (!data) return console.log('No data found to save');
+        try {
+            localStorage.setItem(DATA_KEY, JSON.stringify(data));
+            console.log(`Data successfully saved to localStorage under key: ${DATA_KEY}`);
+        } catch (err) {
+            console.error("Failed to write data to localStorage:", err);
+        }
+    },
 }
 
-export function writeDB(data, nameDB) {
-    if (!data) return console.log('No data Found');
-    try {
-        fs.writeFileSync(nameDB, JSON.stringify(data));
-        console.log("Data Saved");
-    } catch (err) {
-        console.error("Failed to write data:", err);
-    }
-}
-
-export function updateDB(updatedRecord, nameDB, uniqueIdentifier = 'id') {
-    const existingData = readDB(nameDB);
-
-    if (!existingData) {
-        console.error('No existing data found.');
-        return;
-    }
-    const indexToUpdate = existingData.findIndex(record => record[uniqueIdentifier] === updatedRecord[uniqueIdentifier]);
-
-    if (indexToUpdate === -1) {
-        console.error('Record not found for update.');
-        return;
-    }
-    existingData[indexToUpdate] = { ...existingData[indexToUpdate], ...updatedRecord };
-    writeDB(existingData, nameDB);
-}
+export {db};
